d61792eb7792f02b0485f198c248634a5f7e8b52
adisucipto98 <adii.adisucipto@gmail.com>
2023-11-08 15:24:57 +0700

    Task 1

--- Task1.go
+++ Task1.go
@@ -2,8 +2,25 @@ package main
 
 import "fmt"
 
-func main() {
-	for i := 1; i <= 5; i++ {
-		fmt.Println(i)
+func printSegitiga(tinggi int) {
+	k := 0
+
+	for i := tinggi; i >= 1; i-- {
+		for l := 0; l < k; l++ {
+			fmt.Print(" ")
+		}
+
+		k++
+
+		for j := (i*2 - 1); j >= 1; j-- {
+			fmt.Print("*")
+		}
+
+		fmt.Print("\n")
 	}
+}
+
+func main() {
+	tinggi := 5
+	printSegitiga(tinggi)
 }
