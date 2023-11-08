de1763693dc3493c8794d5038fb2f90666b3930f
adisucipto98 <adii.adisucipto@gmail.com>
2023-11-08 17:07:37 +0700

    Finish Task 2

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
 
+// Task 1
 func printSegitiga(tinggi int) {
 	k := 0
 
@@ -20,7 +26,32 @@ func printSegitiga(tinggi int) {
 	}
 }
 
+// Task 2
+func genPass(password string, level string) {
+	rand.Seed(time.Now().UnixNano())
+
+	if level == "Low" {
+		randomInt := rand.Intn(1000) + 10000
+		fmt.Print(password, randomInt)
+	} else if level == "Med" {
+		randomInt := rand.Intn(1000) + 1000
+		fmt.Print(password, randomInt, "@")
+	} else if level == "Strong" {
+		hurufPertama := password[0:1]
+		hurufKedua := strings.ToUpper(password[1:2])
+		hurufKetiga := password[2:3]
+		hurufKeempat := strings.ToUpper(password[3:4])
+		kata := hurufPertama + hurufKedua + hurufKetiga + hurufKeempat
+		randomInt := rand.Intn(1000) + 1000
+		fmt.Print(kata, "@", randomInt)
+	}
+}
+
 func main() {
+	//Task 1
 	tinggi := 5
 	printSegitiga(tinggi)
+
+	// Task 2
+	genPass("abcd", "Strong")
 }
